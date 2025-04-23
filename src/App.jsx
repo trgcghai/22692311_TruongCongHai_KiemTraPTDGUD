634561be9c98e9433053d9cd0d3660a2bb62623a
TruongCongHai <conghai.tpma@gmail.com>
2025-04-23 10:33:23 +0700

    feat: hien thi danh sách sinh viên mau

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
-    <h1 className='text-red-400'>hello world</h1>
-  )
+    <div className="container mx-auto p-4">
+      <div>
+        <h1 className="text-2xl font-bold text-center mb-4">Danh sach sinh vien</h1>
+        <div>
+          <div className="flex items-center justify-between p-2 border rounded-lg">
+            <div>
+              <p>Ho ten: Nguyen Van A</p>
+              <p>Tuoi: 20</p>
+              <p>Lop: CNTT1</p>
+            </div>
+            <div>
+              <button className="text-lg bg-red-400 text-white rounded-lg px-3 py-2">Xoa</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
